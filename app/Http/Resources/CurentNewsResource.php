b47754bdc91b17f49580186a3a4ad7c92be4f64e
Andrey Zakhvatoshin <mastetandron@gmail.com>
2021-08-29 13:35:00 +0500

    Add resource for user news

---
+++ app/Http/Resources/CurentNewsResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CurentNewsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'title' => $this->title,
+            'body' => $this->body,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
