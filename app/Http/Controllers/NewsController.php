0aacda2221f3d62a0896d1f1954ebd7cd570aae1
Andrey Zakhvatoshin <mastetandron@gmail.com>
2021-08-29 13:29:17 +0500

    Change methods

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,33 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Helpers\NewsHelper;
+use App\Http\Resources\CurentNewsResource;
+use App\Http\Resources\NewsResource;
+use App\Models\News;
 
 class NewsController extends Controller
 {
     /**
-     * Находит тип объекта по параметрам из запроса пользователя
-     *
-     * @return LengthAwarePaginator
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
-    public static function findFromRequest(): LengthAwarePaginator
+    public function index(): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
     {
-        $query = ObjectCharacters::select('*')
-            ->when($id = request()->input('id'), function ($query, $id) {
-                return $query->where('id', $id);
-            })->when($off = request()->input('off'), function ($query, $off) {
-                return $query->where('off', $off);
-            })->when($object_id = request()->input('object_id'), function ($query, $object_id) {
-                return $query->where('object_id', $object_id);
-            })->when($object_character_type_id = request()->input('object_character_type_id'), function ($query, $object_character_type_id) {
-                return $query->where('object_character_type_id', $object_character_type_id);
-            })->when($value = request()->input('value'), function ($query, $value) {
-                return $query->where('value', 'LIKE', '%' . $value . '%');
-            })->when($sort = request()->input('sort'), function ($query, $sort) {
-                $sort_direction = Str::startsWith($sort, '-') ? 'DESC' : 'ASC';
-                $sort_column = ltrim($sort, '-');
-                return $query->orderBy($sort_column, $sort_direction);
-            });
-        return $query->paginate();
+        return NewsResource::collection(NewsHelper::findFromRequest());
+    }
+
+    /**
+     * @param int $id
+     * @return CurentNewsResource
+     */
+    public function show(int $id): CurentNewsResource
+    {
+        return new CurentNewsResource(News::findOrFail($id));
     }
 }
