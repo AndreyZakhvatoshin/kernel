d8c06ca54c5b8b6bfbdaca5061a1f0cce27def40
Andrey Zakhvatoshin <mastetandron@gmail.com>
2021-08-29 13:34:32 +0500

    Add resource for admin news

---
+++ app/Http/Resources/Admin/CurentNewsResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Admin;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CurentNewsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'title' => $this->title,
+            'body' => $this->body,
+            'status' => $this->status,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
